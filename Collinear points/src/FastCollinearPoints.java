73239d026766120d4c0cbde0b7a0ef3b1b64cc2b
Rishipai34 <rishipai34@users.noreply.github.com>
2020-04-17 00:42:32 +0530

    Bug fixes and some bugs yet to fix in fastcollinear points

--- Collinear points/src/FastCollinearPoints.java
+++ Collinear points/src/FastCollinearPoints.java
@@ -5,39 +5,44 @@ import java.util.Objects;
 public class FastCollinearPoints {
     private  LineSegment[] lines;
     private int linesize = 0;
-    private int mark ;
-    public FastCollinearPoints(Point[] points){
+    private Point[] sorted;
+
+    public FastCollinearPoints(Point[] points) {
         lines = new LineSegment[points.length];
-        for(Point a: points){
-            Comparator<Point> comp = Objects.requireNonNull(a).slopeOrder();
+        for (Point a: points) {
+            Comparator<Point> comp = a.slopeOrder();
             Arrays.sort(points, comp);
-            for(int i=0 ; i < points.length ; i++) {
-                int j;
-                mark = 0;
-                for(j = i + 1; ;j++){
-                    if((j <= points.length-2) && (points[i].slopeTo(points[j]) ==  points[i].slopeTo(points[j+1]))){
-                        continue;
-                    }
-                    else break;
-                }
-                if(j - i >= 4){
+            sorted = points;
+            for (int j = 0; j < points.length; j++) {
+                int i =j;
+                int mark =0;
+                j = check(j);
+                if (j - i >= 2) {
                     LineSegment num = new LineSegment(points[j-1], points[i]);
                     for (LineSegment temp1 : lines) {
-                        if (Objects.equals(temp1, num)) {
+                        if (temp1 == num) {
                             mark = 1;
                             break;
                         }
                     }
                     if (mark == 0) {
-                        lines[linesize] = new LineSegment(points[i],points[j-1]);
-                        if(linesize <= points.length-2) linesize++;
+                        lines[linesize] = new LineSegment(points[i], points[j-1]);
+                        if (linesize <= points.length-2) linesize++;
                         else break;
                     }
-                    mark = 0;
-                    i = j;
                 }
             }
         }
+    }
+
+    // Checks the collinear points
+    private int check(int j) {
+        int bar = j;
+        if ((bar <= sorted.length-2) && (sorted[0].slopeTo(sorted[bar]) ==  sorted[0].slopeTo(sorted[bar+1]))) {
+            bar++;
+            bar = check(bar);
+        }
+        return bar;
     }
     // the number of line segments
     public int numberOfSegments() {
