018dba3ac8056b8d25b422ba53f1dedd356a14ed
Rishipai34 <rishipai34@users.noreply.github.com>
2020-04-19 21:16:29 +0530

    Trying new ways to fix it

--- Collinear points/src/FastCollinearPoints.java
+++ Collinear points/src/FastCollinearPoints.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
@@ -10,25 +12,21 @@ public class FastCollinearPoints {
     public FastCollinearPoints(Point[] points) {
         lines = new LineSegment[points.length];
         for (Point a: points) {
-            Comparator<Point> comp = a.slopeOrder();
-            Arrays.sort(points, comp);
             sorted = points;
+            Comparator<Point> comp = a.slopeOrder();
+            Arrays.sort(sorted, comp);
+            /*for (Point temp1 : sorted) {
+                StdOut.println(temp1.toString());
+            }*/
             for (int j = 0; j < points.length; j++) {
-                int i =j;
-                int mark =0;
+                int i = j;
                 j = check(j);
                 if (j - i >= 2) {
                     LineSegment num = new LineSegment(points[j-1], points[i]);
-                    for (LineSegment temp1 : lines) {
-                        if (temp1 == num) {
-                            mark = 1;
-                            break;
-                        }
-                    }
-                    if (mark == 0) {
+                    if(!Arrays.asList(lines).contains(num))
+                    {
                         lines[linesize] = new LineSegment(points[i], points[j-1]);
                         if (linesize <= points.length-2) linesize++;
-                        else break;
                     }
                 }
             }
